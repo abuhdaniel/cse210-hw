b0d5ecaad782414811c06f532be29e7207f9c8e5
Daniel-png06 <dabuh@byupathway.edu>
2026-03-02 18:59:31 +0100

    program.cs week01/exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.Write("Enter your first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int result = AddNumbers(num1, num2);
+
+        Console.WriteLine($"The sum of the two numbers is {result}");
+    }
+
+    static int AddNumbers(int number1, int number2)
+    {
+        return number1 + number2;
     }
 }
