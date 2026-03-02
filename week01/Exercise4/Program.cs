cfa0b6147bd2cbf68efdf85ab4623aaa2e69b75d
Daniel-png06 <dabuh@byupathway.edu>
2026-03-02 18:53:59 +0100

    program.cs week01/Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<string> names = new List<string>();
+
+        string userInput = "";
+
+        Console.WriteLine("Enter names (type 'done' when finished):");
+
+        while (userInput.ToLower() != "done")
+        {
+            Console.Write("Enter a name: ");
+            userInput = Console.ReadLine();
+
+            if (userInput.ToLower() != "done")
+            {
+                names.Add(userInput);
+            }
+        }
+
+        Console.WriteLine("\nHere are the names you entered:");
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
     }
 }
