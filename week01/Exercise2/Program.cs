d24567d85b04f4dbae80f868632b504b6cfa353a
Daniel-png06 <dabuh@byupathway.edu>
2026-03-02 18:32:21 +0100

    program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your score: ");
+        int score = int.Parse(Console.ReadLine());
+
+        if (score >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("You got a D!");
+        }
+        else
+        {
+            Console.WriteLine("You got an F.");
+        }
     }
 }
