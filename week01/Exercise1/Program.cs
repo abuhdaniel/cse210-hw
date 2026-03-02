b62ded384f989e1b75e810007f19cce09899b90f
Daniel-png06 <dabuh@byupathway.edu>
2026-03-02 16:46:50 +0100

    program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Hello {name}!");
+        Console.WriteLine($"Your favorite number is {number}.");
+        Console.WriteLine($"Did you know that {number} squared is {number * number}?");
     }
 }
