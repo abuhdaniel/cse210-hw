8437eb50055ded29b661fc65c7eebd557bba872d
Daniel-png06 <dabuh@byupathway.edu>
2026-03-28 19:14:47 +0100

    Address.cs Customer.cs Order.cs Product.cs Program.cs

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Order 1 (NIGERIA)
+        Address address1 = new Address("10 Achinna street", "Oyigbo", "Rivers", "Nigeria");
+        Customer customer1 = new Customer("Abuh Daniel", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Laptop", "P001", 800, 1));
+        order1.AddProduct(new Product("Mouse", "P002", 25, 2));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order1.CalculateTotalCost());
+
+        Console.WriteLine("------------------------");
+
+        // Order 2 (International)
+        Address address2 = new Address("45 King Rd", "London", "N/A", "UK");
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Phone", "P003", 600, 1));
+        order2.AddProduct(new Product("Charger", "P004", 20, 3));
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order2.CalculateTotalCost());
     }
 }
